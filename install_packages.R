4bd26b0d85487fb092838962789e8caad146c43c
Carlos Camilleri-Robles <134629785+ccarloscr@users.noreply.github.com>
2025-03-31 18:07:07 +0200

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,15 +1,22 @@
 #!/usr/bin/env Rscript
 
-
+# Set CRAN mirror
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 
+# Set FFTW environment
 Sys.setenv(PKG_CONFIG_PATH = paste0(Sys.getenv("CONDA_PREFIX"), "/lib/pkgconfig"))
 
+# Install and load ffwtools
 install.packages("fftwtools", configure.args = paste0("--with-fftw=", Sys.getenv("CONDA_PREFIX")))
 library(fftwtools)
 
+# Install and load EBImage
 BiocManager::install("EBImage")
+library(EBImage)
+
+# Install and load optparse
 install.packages("optparse")
+library(optparse)
 
 
 # Vectors containing package names
@@ -21,13 +28,13 @@ if (!requireNamespace("BiocManager", quietly = TRUE)) {
   install.packages("BiocManager")
 }
 
-# Funcion to install cran packages
+# Function to install cran packages
 install_cran <- function(pkg) {
   if (!requireNamespace(pkg, quietly = TRUE)) {
     install.packages(pkg, dependencies = TRUE)}
 }
 
-# Funcion to install bioconductor packages
+# Function to install bioconductor packages
 install_bioc <- function(pkg) {
   if (!requireNamespace(pkg, quietly = TRUE)) {
     BiocManager::install(pkg, dependencies = TRUE)}
@@ -45,6 +52,3 @@ load_library <- function(pkg) {
 # Loading loop
 lapply(cran_packages, load_library)
 lapply(bioc_packages, load_library)
-
-library(EBImage)
-library(optparse)
